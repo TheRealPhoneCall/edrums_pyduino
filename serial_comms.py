f95f4cf8bdd47be22812e73c636af1abfb6975d5
Daryl Pongcol <pongcoldaryl@gmail.com>
2017-03-26 22:54:10 +0800

    Adding read to serial_comms.py

--- serial_comms.py
+++ serial_comms.py
@@ -9,12 +9,23 @@ print ser
 def serial_send(ser, data):
     ser.write(data.encode())
 
+def serial_recv(ser):
+    return ser.readline()
+
 def main():
-    data = "{" + "{9600,1000,100,80,127,0}," + "{3,4,5,10,13,16}," + \
-                 "{0,1,2,3,4,5}," + "{30,40,50,120,120,60}," + \
-                 "{76,77,78,79,80,81}" + "}"
-    print data
-    serial_send(ser, data)
+    try:
+        data = "{" + "{9600,1000,100,80,127,0}," + "{3,4,5,10,13,16}," + \
+                    "{0,1,2,3,4,5}," + "{30,40,50,120,120,60}," + \
+                    "{76,77,78,79,80,81}" + "}"
+        print data
+        serial_send(ser, data)
+        while True:
+            msg_recvd = serial_recv(ser)
+            print msg_recvd
+    except KeyboardInterrupt:
+        print "Keyboard interrupted."
+        print "Serial reading is now terminated."
+
 
 if __name__ == '__main__':
     main()
