6f9a81046ee47a92bc912d0ac3aa42cb76d9a42b
Daryl Pongcol <pongcoldaryl@gmail.com>
2017-04-03 11:23:04 +0800

    Refactoring directory structure

--- start_server.sh
+++ start_server.sh
@@ -4,4 +4,4 @@
 STRING="Starting local git server"
 echo $STRING
 
-git daemon --base-path="C:\Users\Lenovo\Documents\_BERDWARE\7.0_BerDrums\_testing" --export-all
+git daemon --base-path="C:\Users\Lenovo\Documents\_BERDWARE\7.0_BerDrums\version_pyduino" --export-all
