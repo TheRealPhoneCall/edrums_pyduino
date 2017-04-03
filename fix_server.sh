9b9efb85773d48c6866ecf9afa691f1b23786a2e
Daryl Pongcol <pongcoldaryl@gmail.com>
2017-04-04 00:35:59 +0800

    Migrating MIDI computations on python rather than C

--- fix_server.sh
+++ fix_server.sh
@@ -4,6 +4,6 @@
 STRING="Fixing local git server"
 echo $STRING
 
-git config --global pack.windowMemory "100m"
+git config --global pack.windowMemory "200m"
 git config --global pack.threads "1"
-git config --global pack.packSizeLimit "100m"
+git config --global pack.packSizeLimit "200m"
